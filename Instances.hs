5f7dcb9d4e82d0f210bd691cdee8bd56218fda52
Ben Clifford <benc@hawaga.org.uk>
2014-07-17 16:25:44 +0100

    fix to compile (untested) against dns 1.4.0
    this turns on language features i'm not massively
    sure about using. maybe theres a better way?

--- Instances.hs
+++ Instances.hs
@@ -10,7 +10,7 @@ deriving instance Show ResolvConf
 deriving instance Eq ResolvConf
 deriving instance Show FileOrNumericHost
 deriving instance Eq FileOrNumericHost
-deriving instance Ord RDATA
+deriving instance (Ord o) => Ord (RD o)
 -- deriving instance Ord ResourceRecord
 deriving instance Ord TYPE
 
