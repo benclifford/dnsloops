74de7eb354cd8c87225cff9c93449636c2f1bc02
Ben Clifford <benc@hawaga.org.uk>
2014-10-18 12:26:36 +0100

    displayDuplicateRRSets should only display duplicate RRSets rather than all

--- Rules/DuplicateRRs.hs
+++ Rules/DuplicateRRs.hs
@@ -26,8 +26,9 @@ displayDuplicateRRSets = do
 
   let sortedByQ = sortBy (compare `on` fst) filtered
   let groupedByQ = groupBy ( (==) `on` fst) sortedByQ
+  let duplicatesByQ = filter (\l -> length l > 1) groupedByQ
 
-  forM_ groupedByQ $ \g -> do
+  forM_ duplicatesByQ $ \g -> do
     let (GetRRSetQuery name typ,_) = head g -- There must be at least one group because this somes from groupBy, and the fst element should be the same for all elements in the group
     putIO $ (unpack name) ++ "/" ++ (show typ) ++ ":"
     forM_ g $ \(_,GetRRSetAnswer a) -> case a of
